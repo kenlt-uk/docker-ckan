884e2b1353d158ad11ffc37d1896dba0b4f935b3
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2019-11-11 16:17:41 +0000

    Remove postgis installations as already installed

--- postgresql/docker-entrypoint-initdb.d/30_init_postgis.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE template_postgis;
-UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
-
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS postgis_topology;
-CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
