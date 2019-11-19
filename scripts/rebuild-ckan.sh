c0768f5a0cdba4c50bda51e090008ba6de118a04
Ken Tsang <kenlt.uk@gmail.com>
2019-11-19 11:03:20 +0000

    Remove alpine-geos build
    Using kentsang/alpine-geos until alpine fixes the geos dependency

--- scripts/rebuild-ckan.sh
+++ scripts/rebuild-ckan.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-(cd alpine-geos && docker build -t alpine-geos .)
 (cd ckan-base && docker build -t alphagov/ckan-base:2.7 -f 2.7/Dockerfile .)
 (cd ckan-dev && docker build -t alphagov/ckan-dev:2.7 -f 2.7/Dockerfile .)
 (cd ckan && docker build -t alphagov/ckan:latest -f Dockerfile.dev .)
