c9cb465803bd102f9adb42be1bf1f399033c86a5
Ken Tsang <kenlt.uk@gmail.com>
2019-11-19 10:50:13 +0000

    Add ckan-postdev Dockerfile

---
+++ ckan-postdev/docker-entrypoint.d/post_dev_tasks.sh
@@ -0,0 +1,7 @@
+# Add post dev tasks such as changing config settings
+
+# For example-
+
+# echo "Loading test settings into harvest test-core.ini"
+# paster --plugin=ckan config-tool $SRC_EXTENSIONS_DIR/ckanext-harvest/test-core.ini \
+#     "ckan.harvest.mq.hostname = $TEST_CKAN_REDIS_HOSTNAME"
--- ckan-postdev/setup/setup_tests.sh
+++
@@ -1,5 +0,0 @@
-# Update test-core.ini in harvest
-
-echo "Loading test settings into harvest test-core.ini"
-paster --plugin=ckan config-tool $SRC_EXTENSIONS_DIR/ckanext-harvest/test-core.ini \
-    "ckan.harvest.mq.hostname = $TEST_CKAN_REDIS_HOSTNAME"
