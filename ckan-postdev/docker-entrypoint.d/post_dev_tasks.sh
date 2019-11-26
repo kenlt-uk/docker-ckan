7038ac3d326c31f21493c87f6392732959fcdb93
Ken Tsang <kenlt.uk@gmail.com>
2019-11-26 18:47:07 +0000

    Update ckan scripts

--- ckan-postdev/docker-entrypoint.d/post_dev_tasks.sh
+++ ckan-postdev/docker-entrypoint.d/post_dev_tasks.sh
@@ -2,7 +2,6 @@
 
 echo "================ post dev tasks ================"
 
-echo "Loading test settings into ckan test-core.ini"
-paster --plugin=ckan config-tool $SRC_DIR/ckan/test-core.ini \
-    "ckan.harvest.mq.hostname = $TEST_CKAN_REDIS_HOSTNAME" \
-    "ckanext.dcat.base_uri = $TEST_CKAN_SITE_URL"
+# echo "Loading test settings into ckan test-core.ini"
+# paster --plugin=ckan config-tool $SRC_DIR/ckan/test-core.ini \
+#     "ckan.harvest.mq.hostname = $TEST_CKAN_REDIS_HOSTNAME"
