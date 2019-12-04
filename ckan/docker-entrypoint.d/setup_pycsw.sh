78e23ecd82acee85fe2c360c09984f5f0011987a
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2019-12-04 16:32:39 +0000

    Add CSW setup to Dockerfile.dev in ckan

---
+++ ckan/docker-entrypoint.d/setup_pycsw.sh
@@ -0,0 +1,12 @@
+# #!/bin/bash
+
+echo "====== set up pycsw ======"
+
+cd $SRC_EXTENSIONS_DIR/ckanext-spatial
+paster --plugin=ckanext-spatial ckan-pycsw setup -p $APP_DIR/pycsw.cfg
+
+echo "Update pycsw abstract index to allow for larger records"
+PGPASSWORD=ckan psql -h db -U ckan -c "DROP INDEX ix_records_abstract;CREATE INDEX ix_records_abstract ON records((md5(abstract)));"
+
+# run cronjob 
+crond -l 2 -L /var/log/ckan/cron.log
