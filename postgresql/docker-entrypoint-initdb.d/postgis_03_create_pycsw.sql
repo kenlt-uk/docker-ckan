7c4ea6262f860f04a1c45843c34d4e0a3cde8f4d
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2019-12-04 16:35:57 +0000

    Remove drop records abstract from postgis script

--- postgresql/docker-entrypoint-initdb.d/postgis_03_create_pycsw.sql
+++ postgresql/docker-entrypoint-initdb.d/postgis_03_create_pycsw.sql
@@ -1,6 +1,3 @@
 -- this needs to be run after postgis.sh script has run, hence postgis_ suffix
 
 CREATE DATABASE pycsw TEMPLATE template_postgis OWNER ckan ENCODING 'utf-8';
-
--- Update pycsw abstract index to allow for larger records
-DROP INDEX ix_records_abstract;CREATE INDEX ix_records_abstract ON records((md5(abstract)));
