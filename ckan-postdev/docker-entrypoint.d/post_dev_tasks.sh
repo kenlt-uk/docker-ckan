e0ae9423f902348b899a53711e96e20f7ef580bc
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2019-12-13 11:42:35 +0000

    Add args to stop cronjobs running

--- ckan-postdev/docker-entrypoint.d/post_dev_tasks.sh
+++ ckan-postdev/docker-entrypoint.d/post_dev_tasks.sh
@@ -3,6 +3,16 @@
 
 echo "================ post dev tasks ================"
 
+if [ $DISABLE_CRON = 1 ]; then
+    echo "Stopping cronjobs from running..."
+    crontab -l > cron_backup.txt
+    crontab -r
+
+    echo "*** Restore cronjobs with this command"
+    echo "crontab cron_backup.txt"
+fi
+
+# Example for changing test config setting
 # echo "Loading test settings into ckan test-core.ini"
 # paster --plugin=ckan config-tool $SRC_DIR/ckan/test-core.ini \
 #     "ckan.harvest.mq.hostname = $TEST_CKAN_REDIS_HOSTNAME"
