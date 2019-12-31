28641b18ab071a31ada53dbe93cf1b5927a92ed9
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2019-12-31 15:10:00 +0000

    Create postgis extension in ckan_test db

--- ckan/docker-entrypoint.d/setup_spatial.sh
+++ ckan/docker-entrypoint.d/setup_spatial.sh
@@ -3,3 +3,5 @@
 echo "====== Set up Spatial database ======"
 
 paster --plugin=ckanext-spatial spatial initdb -c "$CKAN_INI"
+
+PGPASSWORD=ckan psql -h db -U ckan -d ckan_test -c "CREATE EXTENSION IF NOT EXISTS postgis;"
