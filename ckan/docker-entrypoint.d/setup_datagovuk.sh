d19155f64c02e56e4a38398bd4d2c3a26a93ce49
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-01-08 17:02:03 +0000

    Update datagovuk test.ini in src_extensions not src

--- ckan/docker-entrypoint.d/setup_datagovuk.sh
+++ ckan/docker-entrypoint.d/setup_datagovuk.sh
@@ -3,5 +3,5 @@
 echo "====== Set up Data.gov.uk ======"
 
 echo "Update datagovuk SQLAlchemy URL"
-paster --plugin=ckan config-tool $SRC_DIR/ckanext-datagovuk/test.ini \
+paster --plugin=ckan config-tool $SRC_EXTENSIONS_DIR/ckanext-datagovuk/test.ini \
     "sqlalchemy.url = $TEST_CKAN_SQLALCHEMY_URL"
