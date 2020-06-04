fbf7e816c4fa6882c56fbe565b1b93258cb10e70
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-06-04 16:09:14 +0100

    Update harvest and dcat dependencies

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -25,7 +25,7 @@ pushd src/$SRC_DIR
 git clone --branch ckan-$CKAN_VERSION https://github.com/$CKAN_FORK/ckan
 
 git clone --branch $DATAGOVUK_BRANCH https://github.com/alphagov/ckanext-datagovuk
-git clone https://github.com/alphagov/ckanext-harvest
+git clone https://github.com/ckan/ckanext-harvest
 git clone --branch dgu-fixes https://github.com/alphagov/ckanext-spatial
 git clone https://github.com/ckan/ckanext-dcat
 git clone https://github.com/geopython/pycsw.git --branch 2.4.0 
