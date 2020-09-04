05ce254b49bb9bcd7d12fc0df8ceeb425830d68d
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-09-04 10:13:01 +0100

    Update script with test data

--- ckan-main/docker-entrypoint.d/setup_datagovuk.sh
+++ ckan-main/docker-entrypoint.d/setup_datagovuk.sh
@@ -5,3 +5,7 @@ echo "====== Set up Data.gov.uk ======"
 echo "Update datagovuk SQLAlchemy URL"
 paster --plugin=ckan config-tool $SRC_EXTENSIONS_DIR/ckanext-datagovuk/test.ini \
     "sqlalchemy.url = $TEST_CKAN_SQLALCHEMY_URL"
+
+echo "Setup test data"
+paster --plugin=ckanext-datagovuk remove_dgu_test_data -c $CKAN_INI
+paster --plugin=ckanext-datagovuk create_dgu_test_data -c $CKAN_INI
