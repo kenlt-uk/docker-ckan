20fd177fe0bdb7a2f39f8494419b92d2763b91e3
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2021-01-14 13:03:30 +0000

    Don't set solr_url to test solr url
    Need to create the test solr index first before setting the test solr url

--- ckan-main/docker-entrypoint.d/2.9/setup_datagovuk.sh
+++ ckan-main/docker-entrypoint.d/2.9/setup_datagovuk.sh
@@ -5,7 +5,7 @@ echo "====== Set up Data.gov.uk ======"
 echo "Update datagovuk SQLAlchemy URL"
 ckan config-tool $SRC_EXTENSIONS_DIR/ckanext-datagovuk/test.ini \
     "sqlalchemy.url = $TEST_CKAN_SQLALCHEMY_URL" \
-    "solr_url = $TEST_CKAN_SOLR_URL" \
+    # "solr_url = $TEST_CKAN_SOLR_URL" \
     "ckan.redis.url = $TEST_CKAN_REDIS_URL"
 
 echo "Setup test data"
